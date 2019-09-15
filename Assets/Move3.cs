117fbb0cbd7aba37758582c29f1e96ecb07f868e
DibriyD <ddibrij@gmail.com>
2019-09-15 16:23:12 +0300

    Скрипты движения
    1) Move.cs- движение через rigidbody
    2) Move2.cs- движение через коллайдер
    3) Move2.cs- движение всей сцены

---
+++ Assets/Move3.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move3 : MonoBehaviour
+{
+    private Collider2D collider;
+    [SerializeField] private float speed = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        collider = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float move = Input.GetAxisRaw("Horizontal");
+        if (move == (-1))
+        {
+            collider.transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+        else if (move ==1)
+        {
+            collider.transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+    }
+}
